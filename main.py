08bbc5d05bcf038e019cf579fa436d3e443376e4
Louis HU <enderpro10002@gmail.com>
2026-02-07 10:48:36 +0100

    smth

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
-import pymongo
+from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 
-@app.route("/test")
+mongo = os.getenv('MONGO_URI')
+client = MongoClient(mongo)
+db =client.get_database("Guide")
+
+@app.route("/")
 def test():
-    return render_template('index.html')
+    guide_data = list(db['Guide'].find({}))
+    return render_template('index.html', guides = guide_data)
 
 app.run(host = '0.0.0.0', port=81)
