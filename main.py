c8a73233f48e8a6e1234f6ce970ea2a8e02ecf97
Ender10002 <enderpro10002@gmail.com>
2026-04-04 10:51:04 +0200

    another commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from flask import Flask, render_template, request, session, redirect, url_for
 from pymongo import MongoClient
 import os
@@ -16,22 +17,23 @@ def test():
     guide_data = list(db['guides'].find({}))
     return render_template('index.html', guides = guide_data)
 
-@app.route("/login", methods=['POST', 'GET'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    if request.method == "GET":
+    if request.method == "POST":
         db_users = db["users"]
         user = db_users.find_one({"pseudo" : request.form['utilisateur']})
         if user:
             if bcrypt.checkpw(request.form['mot_de_passe'].encode('utf-8'), user["password"]):
                 session['role'] = user['role']
                 session['user'] = user['pseudo']
+                return redirect("/")
+                
             else:
                 return render_template("front/login.html", erreur = "les mots de passes ne correspondent pas")
-        return render_template('front/login.html', erreur = "les mots de passes ne correspondent pas")
-    else:
-        return render_template('front/login.html')
+        else:
+            return render_template('front/login.html')
 
-@app.route("/signup")
+@app.route("/signin")
 def signin():
     if request.method == 'POST':
         return render_template('signin.html', erreur = "le nom d'utilisateur existe déjà")
@@ -58,4 +60,30 @@ def signin():
         else:
             return render_template('signin.html', erreur = "les mots de passe ne correspondent pas")
         
+##########ADMIN##########
+
+@app.route('/admin')
+def admin():
+    guide_data = list(db['guides'].find({}))
+    users = list(db['users'].find({}))
+    if 'util' in session and session['role'] == 'admin':
+        return render_template('back/back_accuei.html', guides=guide_data, users=users)
+    else:
+        return render_template('index.html', erreur="Vous n'avez pas les droits d'accès", guides=guide_data, users=users)
+    
+@app.route('/admin/update_role/<user_id>')
+def update_role(user_id):
+    if 'util' in session and session['role'] == 'admin':
+        new_role = request.form.get('role')
+
+        db['dresseurs'].update_one({"_id": ObjectId(user_id)}, {"$set": {"role": new_role}})
+
+    return redirect(url_for('admin'))
+
+@app.route('/admin/delete_user/<user_id>')
+def delete_user(user_id):
+    if 'util' in session and session['role'] == 'admin':
+        db['users'].delete_one({"_id": ObjectId(user_id)})
+    return redirect(url_for('admin'))
+
 app.run(host = '0.0.0.0', port=81)
